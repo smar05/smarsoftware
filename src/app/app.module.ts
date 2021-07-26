d85be42435d885220a4fe595524456071a10d82d
smar05 <mantillasanchezr@gmail.com>
2021-07-26 00:19:55 -0500

    Diseño inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
